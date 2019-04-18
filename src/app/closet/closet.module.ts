125847844a33d17827e734e9917d97026c35c4fe
Sarah Chin <sarahchin04@gmail.com>
2019-04-18 01:16:53 -0400

    Added CSS sidenav

--- src/app/closet/closet.module.ts
+++ src/app/closet/closet.module.ts
@@ -1,25 +1,25 @@
+import { MaterialModule } from './../material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 import { ListComponent } from './list/list.component';
 import { UploadComponent } from './upload/upload.component';
 import { OutfitComponent } from './outfit/outfit.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-
 import { ClosetRoutingModule } from './closet-routing.module';
-import { MaterialModule } from '../material.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { ChipsInputComponent } from './upload/chips-input/chips-input.component';
 
 @NgModule({
-  declarations: [ListComponent, UploadComponent, OutfitComponent, SidenavComponent],
+  declarations: [ListComponent, UploadComponent, OutfitComponent, ChipsInputComponent],
   imports: [
     CommonModule,
     ClosetRoutingModule,
     FormsModule,    //added here too
     ReactiveFormsModule, //added here too
-    MaterialModule
+    MaterialModule,
+    MatInputModule
   ]
 })
 export class ClosetModule { }
