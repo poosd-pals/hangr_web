4cdcba6e0dc6201d84c8f58e207cccc7873047e5
Sarah Chin <sarahchin04@gmail.com>
2019-04-18 18:06:29 -0400

    Finished sidebar styling

--- src/app/closet/list/list.component.ts
+++ src/app/closet/list/list.component.ts
@@ -2,21 +2,41 @@ import { Clothing } from './../clothing';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../../api/api.service';
 
+
+
+
+export interface Tag {
+    name: string;
+  }
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 
+
 export class ListComponent implements OnInit {
+
+    selectable = true;
+    removable = true;
 
     // long text is the "add clothing" text when the button is expanded.
     addButtonLongText: boolean;
     clothing: Object;
 
+    tags: Tag[] = [
+        {name: 'Casual'},
+        {name: 'Business'},
+        {name: 'Summer'},
+      ];
+
+    /* tags = ['Professional', 'Casual', 'Winter', 'Summer']; */
+
     // Empty array 'closet'.
     closet: any[] = [];
 
+    // Hardcoded array of categories for sidenav.
     categories = ['Accessories', 'Bags', 'Dresses', 'Intimates & Sleepwear', 
         'Jackets & Coats', 'Jeans', 'Jewelry', 'Pants', 'Shoes', 'Shorts', 'Skirts', 'Sweaters', 'Swim', 'Tops', 'Other'];
 
@@ -26,6 +46,14 @@ export class ListComponent implements OnInit {
         // Populates closet with call to api getClothing.
         this.closet = this.api.getClothing();
     }
+
+    remove(tag: Tag): void {
+        const index = this.tags.indexOf(tag);
+    
+        if (index >= 0) {
+          this.tags.splice(index, 1);
+        }
+      }
 
     ngOnInit() {}
 
