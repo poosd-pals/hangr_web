ac9f6c385cb1c8152e25b66f62e9f015d9f76904
jin <winterchocolatte@gmail.com>
2019-04-20 01:15:44 -0400

    Made chip values bind to FormGroup.

--- src/app/closet/upload/upload.component.ts
+++ src/app/closet/upload/upload.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ChipsInputComponent } from './chips-input/chips-input.component';
 
 import { ApiService } from './../../api/api.service';
+
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material';
 
 @Component({
   selector: 'app-upload',
@@ -11,20 +15,34 @@ import { ApiService } from './../../api/api.service';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  // form stuff
   clothingForm: FormGroup;
   submitted = false;
   success = false;
 
+  // chips stuff
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+
+  // Enter, comma
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  // Array for the chips - tags and colors
+  tags = [];
+  colors = [];
+
   // Default image
   imgUrl: String = "../../../assets/image-upload-icon.png";
 
-  constructor(private api: ApiService, private fb: FormBuilder) { 
+  constructor(private api: ApiService, /*private afStorage: AngularFireStorage,*/ private fb: FormBuilder) { 
     this.clothingForm = this.fb.group({
       name: ['', Validators.required],
       category: ['', Validators.required],
       wearsBeforeWash: ['', Validators.required],
-      colors: ['', Validators.required],
-      tags: ['']
+      colors: [],
+      tags: []
     });
   }
 
@@ -38,6 +56,7 @@ export class UploadComponent implements OnInit {
           this.imgUrl = event.target.result;
       }
       reader.readAsDataURL((<HTMLInputElement>event.target).files[0]);
+      //this.afStorage.upload('/upload/to/this-path', (<HTMLInputElement>event.target).files[0]);
       console.log((<HTMLInputElement>event.target).files[0]);
   }
   }
@@ -51,12 +70,64 @@ export class UploadComponent implements OnInit {
 
     console.log(this.clothingForm);
 
+    this.clothingForm.patchValue({ colors: this.colors.map(color => color.name) });
+    this.clothingForm.patchValue({ tags: this.tags.map(tag => tag.name) });
+
+    console.log(this.clothingForm.value);
+
     this.api.addClothing(this.clothingForm);
 
     this.success = true;
 
     // TODO: Send to Firebase
-    console.log("form submittd!");
+    console.log("form submitted!");
+  }
+
+  // Chip functions
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add a new tag
+    if ((value || '').trim()) {
+      this.tags.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeTag(tag): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.colors.splice(index, 1);
+    }
+  }
+
+  addColor(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add a new color
+    if ((value || '').trim()) {
+      this.colors.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeColor(color): void {
+    const index = this.colors.indexOf(color);
+
+    if (index >= 0) {
+      this.colors.splice(index, 1);
+    }
   }
 }
 
