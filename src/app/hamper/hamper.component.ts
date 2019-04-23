6504e48f041292daf433ffc292675ad09b383512
Sarah Chin <sarahchin04@gmail.com>
2019-04-22 23:31:18 -0400

    Added hamper

--- src/app/hamper/hamper.component.ts
+++ src/app/hamper/hamper.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ClothingService } from './../api/clothing/clothing.service';
 import { HamperService } from './../api/hamper/hamper.service';
+import { ClothingItem } from './../closet/clothing-model';
 
 @Component({
   selector: 'app-hamper',
@@ -18,13 +19,23 @@ export class HamperComponent implements OnInit {
 
   ngOnInit() {
 
-    // To access hamper item data, it is this.clothingItems[i].data. Then use . operator to access fields
     this.hamperService.getHamper().subscribe(data => {
+        this.clothingItems = data.map(e => {
+            return {id: e.payload.doc.id,
+                    ...e.payload.doc.data()
+                } as ClothingItem;
+            });
+        console.log(this.clothingItems);
+        });
+
+    // To access hamper item data, it is this.clothingItems[i].data. Then use . operator to access fields
+/*     this.hamperService.getHamper().subscribe(data => {
       data.map(dca => {
         this.clothingItems.push(dca.payload.doc);
       });
       console.log(this.clothingItems);
-    });
+
+    }); */
     // var hamperItems = [];
     // this.hamperService.getHamper().subscribe(data => {
     //   data.map((item) => {
@@ -34,4 +45,9 @@ export class HamperComponent implements OnInit {
     // })
   }
 
+  clean(item){
+
+    console.log("Cleaning!")
+    this.hamperService.cleanItem(item);
+  }
 }
